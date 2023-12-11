bf3de187426ed5089a1463cbcf022ede7753ef0f
TamasSomi <>
2023-12-11 11:12:22 +0000

    add like and like details views

--- comments/views.py
+++ comments/views.py
@@ -1,9 +1,9 @@
-from rest_framework import gerenics, permissions
+from rest_framework import generics, permissions
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
 
-class CommentList(generics.ListCreateAPIViews):
+class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
